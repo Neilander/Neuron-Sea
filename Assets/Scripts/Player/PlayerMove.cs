f1f4474bbf3658474c635819cdb1c848b3fa9b10
paofan25 <106793298+paofan25@users.noreply.github.com>
2025-03-27 17:56:23 +0800

    Add Jump Smooth Ani

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -1,44 +1,63 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    [SerializeField] private int moveSpeed=10;
-    private Animator ani;
+    [Header("Movement Settings")] [SerializeField]
+    private float moveSpeed = 10f;
+
+    [SerializeField] private float jumpForce = 5f;
+
+    [Header("Ground Check Settings")] [SerializeField]
+    private Transform groundCheck; // 地面检测点
+
+    [SerializeField] private float groundCheckRadius = 0.2f;
+
+    [SerializeField] private LayerMask groundLayer; // 只检测地面层
+
+    private Animator animator;
+
     private Rigidbody2D rb;
-    public float jumpForce=5f;
-    public bool isGrounded;
+
+    private bool isGrounded;
+
     private void Start(){
-        ani = GetComponent<Animator>();
+        animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
     }
 
-    public void Update(){
-        print(Input.GetAxis("Horizontal"));
-        transform.Translate(Vector2.right * Input.GetAxis("Horizontal") *moveSpeed* Time.deltaTime);
-        PlayerRotate();
-        ani.SetFloat("Speed", Mathf.Abs(Input.GetAxis("Horizontal")));
-        PlayerJump();
+    private void Update(){
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+        animator.SetBool("isGrounded", isGrounded);
+        Move();
+        Rotate();
+        Jump();
     }
 
-    private void PlayerJump(){
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-            isGrounded = false;
-            // ani.SetTrigger("Jump");
-        }
+    private void Move(){
+        float moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+        animator.SetFloat("Speed", Mathf.Abs(moveInput));
     }
 
-    private void PlayerRotate(){
-        if (Input.GetAxis("Horizontal") < 0) {
+    private void Rotate(){
+        float moveInput = Input.GetAxis("Horizontal");
+        if (moveInput < 0)
             transform.localScale = new Vector3(-1, 1, 1);
-        }
-        
-        else if (Input.GetAxis("Horizontal") > 0) {
+        else if (moveInput > 0)
             transform.localScale = new Vector3(1, 1, 1);
+    }
+
+    private void Jump(){
+        if (Input.GetButtonDown("Jump")&& isGrounded) {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            animator.SetBool("isGrounded", false);
         }
     }
-    
+
+    private void OnCollisionEnter2D(Collision2D collision){
+        if (collision.gameObject.CompareTag("Ground")) {
+            isGrounded = true;
+            animator.SetBool("isGrounded", true);
+        }
+    }
 }
--- Assets/Scripts/Player/StateTrans.cs
+++ Assets/Scripts/Player/StateTrans.cs
@@ -10,12 +10,23 @@ public class StateTrans : StateMachineBehaviour
       //如果当前状态是animationName则执行什么样的逻辑
       if(stateInfo.IsName(animationName))
          Debug.Log("进入Blend Tree状态了");
-      animator.gameObject.transform.localScale = Vector3.one;//改变动作对象的所有信息
+      // animator.gameObject.transform.localScale = Vector3.one;//改变动作对象的所有信息
+      if (stateInfo.IsName("Idle")) // Idle 状态，角色在地面
+      {
+         animator.SetBool("isGrounded", true);
+         Debug.Log("进入 Idle 状态，设置 isGrounded = true");
+      }
+      else if (stateInfo.IsName("Jump") || stateInfo.IsName("Fall")) // 起跳或下落
+      {
+         animator.SetBool("isGrounded", false);
+         Debug.Log("进入 Jump/Fall 状态，设置 isGrounded = false");
+      }
    }
 
    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex){
       if (stateInfo.IsName("Blend Tree"))
          Debug.Log("退出Blend Tree状态了");
+      
    }
 
    public override void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex){
@@ -29,6 +40,16 @@ public class StateTrans : StateMachineBehaviour
    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex){
       if (stateInfo.IsName("Blend Tree"))
          Debug.Log("处于Blend Tree状态了");
+      if (stateInfo.IsName("Chan_idle")) // Idle 状态，角色在地面
+      {
+         animator.SetBool("isGrounded", true);
+         Debug.Log("进入 Idle 状态，设置 isGrounded = true");
+      }
+      else if (stateInfo.IsName("Chan_Jump")) // 起跳或下落
+      {
+         animator.SetBool("isGrounded", false);
+         Debug.Log("进入 Jump/Fall 状态，设置 isGrounded = false");
+      }
    }
 //子状态进入时
    public override void OnStateMachineEnter(Animator animator, int stateMachinePathHash){
