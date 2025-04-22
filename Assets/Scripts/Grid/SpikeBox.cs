e566e94649c16510775cf3ec81f5d97fcf3213f4
Neilander <yw6472@nyu.edu>
2025-04-22 09:53:42 -0400

    尖刺

--- Assets/Scripts/Grid/SpikeBox.cs
+++ Assets/Scripts/Grid/SpikeBox.cs
@@ -4,7 +4,17 @@ using UnityEngine;
 
 public class SpikeBox : MonoBehaviour
 {
+    /*
     private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerController>() != null)
+        {
+            Debug.Log("玩家触碰了尖刺");
+            PlayerDeathEvent.Trigger(gameObject, DeathType.Spike);
+        }
+    }*/
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.GetComponent<PlayerController>() != null)
         {
