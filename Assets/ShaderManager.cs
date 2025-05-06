d1868b91fc9d190e8f180ee5db6d3c49cd990d75
Meiyou <1139964041@qq.com>
2025-05-07 00:21:33 +0800

    死亡特效

--- Assets/ShaderManager.cs
+++ Assets/ShaderManager.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class ShaderManager : MonoBehaviour
 {
+    public Material screenDissolve;
+
+    private void Start()
+    {
+        screenDissolve.SetFloat("_KaiShiShiJian", -10f);
+    }
+
     private void Update()
     {
         Shader.SetGlobalFloat("_UnscaledTime", Time.unscaledTime);
