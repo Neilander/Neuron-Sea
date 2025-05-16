0d750115c959673b0ed2d62251e9b29ea9039e11
Meiyou <1139964041@qq.com>
2025-05-17 00:16:51 +0800

    微调

--- Assets/UIConpanion.cs
+++ Assets/UIConpanion.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 public class UIConpanion : MonoBehaviour
 {
     Animator anim;
+    RectTransform rectTransform;
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
         anim.updateMode = AnimatorUpdateMode.UnscaledTime;
+        rectTransform = GetComponent<RectTransform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        anim.SetInteger("MaxScene", levelManager.instance.IsLevelUnlocked(25) ? 3 : levelManager.instance.IsLevelUnlocked(13) ? 2 : 1);
+        int maxScene = levelManager.instance.IsLevelUnlocked(25) ? 3 : levelManager.instance.IsLevelUnlocked(13) ? 2 : 1;
+        anim.SetInteger("MaxScene", maxScene);
+        rectTransform.localPosition = transform.GetChild(maxScene - 1).GetComponent<RectTransform>().localPosition;
+        rectTransform.sizeDelta = transform.GetChild(maxScene - 1).GetComponent<RectTransform>().sizeDelta;
     }
 }
