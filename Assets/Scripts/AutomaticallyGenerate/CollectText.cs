2faaf97552d3340663d5b434d25de654dad30ce0
paofan25 <106793298+paofan25@users.noreply.github.com>
2025-05-25 23:47:15 +0800

    完成

--- Assets/Scripts/AutomaticallyGenerate/CollectText.cs
+++ Assets/Scripts/AutomaticallyGenerate/CollectText.cs
@@ -3,16 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using static Unity.Burst.Intrinsics.X86.Avx;
 
 public class CollectText : MonoBehaviour
 {
     [Header("Animation Settings")]
     [SerializeField] public RectTransform _rectTransform;
-    [SerializeField] private Vector2 _startPosition = new Vector2(-640, 0);
+    [SerializeField] private Vector2 _startPosition = new Vector2(-960, 0);
     [SerializeField] private Vector2 showPosition = new Vector2(0, 0);
     [SerializeField] private float showDuration = 0.8f;
-    [SerializeField] private float stayDuration = 1.5f;
+    [SerializeField] private float stayDuration = 5f;
     [SerializeField] private float hideDuration = 0.6f;
     [SerializeField] private AnimationCurve showCurve = AnimationCurve.EaseInOut(0, 0, 1, 1);
     [SerializeField] private AnimationCurve hideCurve = AnimationCurve.EaseInOut(0, 0, 1, 1);
@@ -20,9 +19,9 @@ public class CollectText : MonoBehaviour
     [Header("LevelName")]
     [SerializeField] public TextMeshProUGUI collectText;
 
-    void Start()
+    public void Start()
     {
-        
+        //设置位置
         _rectTransform.anchoredPosition = _startPosition;
         StartCoroutine(PlayAnimation());
     }
@@ -32,21 +31,21 @@ public class CollectText : MonoBehaviour
     {
         // 第一阶段：弹出动画
         yield return StartCoroutine(MoveCoroutine(
-            startPos: _startPosition,
-            targetPos: showPosition,
-            duration: showDuration,
-            curve: showCurve
+            _startPosition,
+            showPosition,
+            showDuration,
+            showCurve
         ));
 
         // 第二阶段：停留等待
-        yield return new WaitForSeconds(stayDuration);
+        yield return new WaitForSecondsRealtime(stayDuration);
 
         // 第三阶段：收回动画
         yield return StartCoroutine(MoveCoroutine(
-            startPos: showPosition,
-            targetPos: _startPosition,
-            duration: hideDuration,
-            curve: hideCurve
+            showPosition,
+            _startPosition,
+            hideDuration,
+            hideCurve
         ));
 
         // 销毁自身
@@ -59,7 +58,7 @@ public class CollectText : MonoBehaviour
 
         while (elapsed < duration)
         {
-            elapsed += Time.deltaTime;
+            elapsed += Time.unscaledDeltaTime;
             float t = Mathf.Clamp01(elapsed / duration);
             float curvedT = curve.Evaluate(t);
 
